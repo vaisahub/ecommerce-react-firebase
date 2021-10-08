9238c73b8a366c6fd6a281c62ba394127edeeebc
vaisakh <997vaisakh@gmail.com>
2021-10-09 00:23:25 +0530

    splitting comp

---
+++ wear-store/src/components/pages/collection.overview.component.jsx
@@ -0,0 +1,21 @@
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectShopData } from "../../redux/selectors/shop.data.selector";
+import './collection-preview.component.scss';
+import CollectionPreview from './collection-preview.component';
+
+const CollectionOverview = ({ previewData }) => (
+    <div>
+        {previewData.map(({ id, ...data }) => {
+            return (
+                <CollectionPreview key={id} {...data}> </CollectionPreview>
+            )
+        })}
+    </div>
+);
+const mapStateToProps = createStructuredSelector(
+    {
+        previewData: selectShopData
+    }
+)
+export default connect(mapStateToProps)(CollectionOverview)
--- wear-store/src/components/pages/shop.component.jsx
+++ wear-store/src/components/pages/shop.component.jsx
@@ -1,26 +1,15 @@
 import './shop.component.scss'
 import React from 'react';
-import CollectionPreview from './collection-preview.component';
-import { createStructuredSelector } from 'reselect';
-import { selectShopData } from '../../redux/selectors/shop.data.selector';
-import { connect } from 'react-redux';
+import CollectionOverview from './collection.overview.component';
 
-const Shop = ({ previewData }) => (
+const Shop = () => (
 
     <div className="shop-page">
-        {previewData.map(({ id, ...data }) => {
-            return (
-                <CollectionPreview key={id} {...data}> </CollectionPreview>
-            )
-        })}
-    </div>
+        <CollectionOverview />
+    </div >
 
 )
 
-const mapStateToProps = createStructuredSelector(
-    {
-        previewData: selectShopData
-    }
-)
 
-export default connect(mapStateToProps)(Shop);
+
+export default Shop;
